2f61dd47abbb0de579c33a5308664a428fd73304
LZHK1ng <1160313432@qq.com>
2023-11-16 12:58:54 +0800

    feat: 修改README.md

--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 - 1. 内容
 
 - 2. 依赖关系
+
+2. 2023.11.16 08.26
+
+- 开始通过createAsset获取一个文件内容并给此文件创建一个块
+  - 拿到文件内容 re.readFileSync
+  - 获取依赖关系 ast = parser.parse(source)
+  - 用一个dep存起来这些依赖关系
+  - 转成cjs形式 -> transformFromAst(ast, null, {presets: ["env"]})
+
+- 把这些依赖关系合成一个图然后进行遍历createGraph()
+  - 把这些Asset放到一个队列里面 const queue = [createAsset("xxx")]
+  - 循环这个队列 + 这个队列里的路径 -> 拿到当前文件对应的依赖 -> queue.push(child) child = createAssets(path.resolve("xx", relativePath))
+  - 返回一个 queue，这个就是所有文件依赖关联图
+
+- 通过图生成js文件 const graph = createGraph()
+  - 创建一个模版数据 data
+  - 通过ejs.render 将模版和data一起渲染出 code 写入 bundle.js
--- dist/bundle.js
+++ dist/bundle.js
@@ -1,51 +1,67 @@
 (function (modules) {
-    // 基于文件路径找到对应函数
-    function require(id) {
+  // 基于文件路径找到对应函数
+  function require(id) {
 
-        // 找到对应的函数
-        const [fn, mapping] = modules[id]
+    // 找到对应的函数
+    const [fn, mapping] = modules[id]
 
-        const module = {
-            exports: {}
-        }
-
-        // 当调用require要变，现在变为id了
-        function localRequire(filePath) {
-            const id = mapping[filePath]
-            return require(id)
-        }
-
-        fn(localRequire, module, module.exports)
-
-        return module.exports
+    const module = {
+      exports: {}
     }
 
-    // 需要执行当前的入口文件mainjs
-    require(0)
+    // 当调用require要变，现在变为id了
+    function localRequire(filePath) {
+      const id = mapping[filePath]
+      return require(id)
+    }
+
+    fn(localRequire, module, module.exports)
+
+    return module.exports
+  }
+
+  // 需要执行当前的入口文件mainjs
+  require(0)
 
 
 })({
 
-    "0": [function (require, module, exports) {
-        "use strict";
+  "0": [function (require, module, exports) {
+    "use strict";
 
-        var _foo = require("./foo.js");
+    var _foo = require("./foo.js");
 
-        (0, _foo.foo)();
-        console.log("main.js");
-    }, { "./foo.js": 1 }],
+    (0, _foo.foo)();
+    console.log("main.js");
+  }, { "./foo.js": 1 }],
 
-    "1": [function (require, module, exports) {
-        "use strict";
+  "1": [function (require, module, exports) {
+    "use strict";
 
-        Object.defineProperty(exports, "__esModule", {
-            value: true
-        });
-        exports.foo = foo;
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.foo = foo;
 
-        function foo() {
-            console.log("foo");
-        }
-    }, {}],
+    var _bar = require("./bar.js");
+
+    function foo() {
+      console.log("foo");
+      (0, _bar.bar)();
+    }
+  }, { "./bar.js": 2 }],
+
+  "2": [function (require, module, exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.bar = bar;
+
+    function bar() {
+      console.log("bar");
+    }
+  }, {}],
 
 })
